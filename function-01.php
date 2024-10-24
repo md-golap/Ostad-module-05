4a4665a68655093575b811216da99772684ffaf6
md-golap <golapmax@gmail.com>
2024-10-24 06:32:02 -1200

    first commit

---
+++ function-01.php
@@ -0,0 +1,49 @@
+<?php
+
+   function great1(){
+
+        $messege= "hello learning 01";
+        echo $messege;
+   }
+
+
+   function great2(){   
+
+    $messege= "hello learnig 02";
+    echo $messege;
+
+
+
+
+
+
+
+
+   }
+
+
+
+great1();
+echo"\n";
+great2();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
---
+++ function-02.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+$x = 12;
+$y = 15;
+
+
+function addTwo (){
+    $x+$y;
+}
+
+
+
+
+function_addThree(){
+    $x+$y+10;
+}
+
+
+
+
+
+
+?>
